f84506a5248855414876daa14c56336c00fcec36
emelinelawford <emelawford@gmail.com>
2021-11-18 16:02:55 +0000

    Composing Components
    Components inside components.

--- app.js
+++ app.js
@@ -7,4 +7,33 @@ const Header = () => {
   );
 };
 
-ReactDOM.render(<Header />, document.getElementById("root"));
+const Player = () => {
+  return (
+    <div className="player">
+      <span className="player-name">Emeline</span>
+      <Counter />
+    </div>
+  );
+};
+
+const Counter = () => {
+  return (
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <span className="counter-score">35</span>
+      <button className="counter-action increment"> + </button>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="scoreboard">
+      <Header />
+      {/* Players list */}
+      <Player />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
