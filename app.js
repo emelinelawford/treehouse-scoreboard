7501862e73431012383558efb06d3651186cf62e
emelinelawford <emelawford@gmail.com>
2021-11-18 15:46:59 +0000

    Arrow Function with return
    return added

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const Header = () => {
-  <header>
-    <h1>Scoreboard</h1>
-    <span className="stats">Players: 1</span>
-  </header>;
+  return (
+    <header>
+      <h1>Scoreboard</h1>
+      <span className="stats">Players: 1</span>
+    </header>
+  );
 };
 
 ReactDOM.render(<Header />, document.getElementById("root"));
