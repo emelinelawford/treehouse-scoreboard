ee0fea4aa3cc9b10e93dacf232e70c8fc1c670aa
emelinelawford <emelawford@gmail.com>
2021-11-18 15:23:05 +0000

    Use a Component with JSX
    Creating a component that contains JSX tags

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-const desc = 'I just learnt how to create a React node and render it into the DOM'
-const myTitleID = 'main-title'
-const name = 'Emeline'
-const header = (
+function Header() {
+  return (
     <header>
-        <h1 id={myTitleID}> { name }'s First React Element! </h1>
-        <p className="class"> { desc } </p>
+      <h1>Scoreboard</h1>
+      <span className="stats">Players: 1</span>
     </header>
-);
+  );
+}
 
-ReactDOM.render(
-    header,
-    document.getElementById('root')
-);
+ReactDOM.render(<Header />, document.getElementById("root"));
