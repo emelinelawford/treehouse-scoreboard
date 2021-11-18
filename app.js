5f2d8beb65e89e09e90bad8f0543a427c3ce2865
emelinelawford <emelawford@gmail.com>
2021-11-18 14:46:33 +0000

    Embed JS Expressions in JSX
    Using curly brackets to include JS.

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-const title = <h1>My First React Element!</h1>;
-
-const desc = <p>I just learnt how to create a React node and render it into the DOM</p>
-
-const header = React.createElement(
-    'header',
-    null,
-    title,
-    desc
+const desc = 'I just learnt how to create a React node and render it into the DOM'
+const myTitleID = 'main-title'
+const name = 'Emeline'
+const header = (
+    <header>
+        <h1 id={myTitleID}> { name }'s First React Element! </h1>
+        <p className="class"> { desc } </p>
+    </header>
 );
 
 ReactDOM.render(
