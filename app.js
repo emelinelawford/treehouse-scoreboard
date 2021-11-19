3e3fd8c8dd1ab6d95a01b2566c7f795434c56aff
emelinelawford <emelawford@gmail.com>
2021-11-19 16:47:21 +0000

    Create a Stateful Component
    Defined an initial state inside the counter class component

--- app.js
+++ app.js
@@ -34,17 +34,21 @@ const Player = (props) => {
   return (
     <div className="player">
       <span className="player-name">{props.name}</span>
-      <Counter score={props.score} />
+      <Counter />
     </div>
   );
 };
 
 class Counter extends React.Component {
+  state = {
+    score: 0,
+  };
+
   render() {
     return (
       <div className="counter">
         <button className="counter-action decrement"> - </button>
-        <span className="counter-score">{this.props.score}</span>
+        <span className="counter-score">{this.state.score}</span>
         <button className="counter-action increment"> + </button>
       </div>
     );
