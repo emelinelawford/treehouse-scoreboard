5248cb42d438da8fae1476dc912557f89751b42b
emelinelawford <emelawford@gmail.com>
2021-11-19 13:18:52 +0000

    Use Props to Create Reusable Components
    Setting up the Player component to receive props and pass props down to the Counter component.

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const Header = (props) => {
-  console.log(props);
   return (
     <header>
       <h1>{props.title}</h1>
@@ -8,20 +7,20 @@ const Header = (props) => {
   );
 };
 
-const Player = () => {
+const Player = (props) => {
   return (
     <div className="player">
-      <span className="player-name">Emeline</span>
-      <Counter />
+      <span className="player-name">{props.name}</span>
+      <Counter score={props.score} />
     </div>
   );
 };
 
-const Counter = () => {
+const Counter = (props) => {
   return (
     <div className="counter">
       <button className="counter-action decrement"> - </button>
-      <span className="counter-score">35</span>
+      <span className="counter-score">{props.score}</span>
       <button className="counter-action increment"> + </button>
     </div>
   );
@@ -32,7 +31,10 @@ const App = () => {
     <div className="scoreboard">
       <Header title="Scoreboard" totalPlayers={1} />
       {/* Players list */}
-      <Player />
+      <Player name="Emeline" score={50} />
+      <Player name="James" score={55} />
+      <Player name="Emily" score={25} />
+      <Player name="Cat" score={35} />
     </div>
   );
 };
